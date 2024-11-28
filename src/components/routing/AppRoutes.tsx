2a234be91e93c418fae2477de8fbd7870d8e01a2
TakumiUemoto <ue0117tk@gmail.com>
2024-11-28 15:00:36 +0900

    feat: routing

--- src/components/routing/AppRoutes.tsx
+++ src/components/routing/AppRoutes.tsx
@@ -1,20 +1,20 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes
-} from "react-router-dom";
-import App from "../../App";
-import Counter from "../../features/Counter";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import App from '../../App'
+import Counter from '../../features/Counter'
+import Users from '../../features/2_users'
+import UsersSample from '../../features/2_users_sample'
 
 const AppRoutes = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/counter' element={<Counter />} />
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/counter" element={<Counter />} />
+				<Route path="/users" element={<Users />} />
+				<Route path="/users-sample" element={<UsersSample />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
-export default AppRoutes;
+export default AppRoutes
