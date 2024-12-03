76fbd3fe444f5fa47db860a684519d42a6e60d75
Yukiko Ikeda <y.ikeda.cheer@gmail.com>
2024-12-03 19:08:25 +0900

    第1週 カウンターアプリ　指摘反映

--- src/features/Counter/index.tsx
+++ src/features/Counter/index.tsx
@@ -1,83 +1,97 @@
 import { useState } from "react";
-import RadioButton from './RadioButton';
-import { Link } from 'react-router-dom'
+import RadioButton from "./RadioButton";
+import { Link } from "react-router-dom";
+
+type CounterMessageType = "even" | "odd" | "reset" | "initial";
+type RadioOption = {
+  value: number;
+};
+
+// メッセージの情報
+const messageInfo: Record<CounterMessageType, string> = {
+  even: "偶数です。",
+  odd: "奇数です。",
+  reset: "リセットされました。",
+  initial: "ボタンを押してください。",
+};
+
+// ラジオボタンの情報
+const radioInfo: RadioOption[] = [
+  {
+    value: 1,
+  },
+  {
+    value: 3,
+  },
+  {
+    value: 5,
+  },
+];
 
 function Counter(): JSX.Element {
   // カウンターのロジック・状態管理
 
-      //Stateの定義
-      const [num, setNum] = useState<number>(0);
-      const [msg, setMsg] = useState<string>("　");
-      const [selectedValue, setSelectedValue] = useState<number>(1);
-  
-      //ボタンクリック時
-      const onClickButton = () => {
+  //Stateの定義
+  const [num, setNum] = useState<number>(0);
+  const [selectedValue, setSelectedValue] = useState<number>(1);
+  const [messageKey, setMessageKey] = useState<CounterMessageType>("initial");
 
-        setNum(num + 1*selectedValue);
+  //ボタンクリック時
+  const onClickButton = () => {
+    const count = num + selectedValue;
+    setNum(count);
+    setMessageKey(count % 2 === 0 ? "even" : "odd");
+  };
 
-        if(num % 2 === 0){
-            setMsg("奇数です");
-        }else{
-            setMsg("偶数です");
-        }
+  //リセットボタンクリック時
+  const onClickResetButton = () => {
+    setNum(0);
+    setMessageKey("reset");
+  };
 
-      }
-  
-      //リセットボタンクリック時
-      const onClickResetButton = () => {
-        setNum(0);
-        setMsg("　");
-      }
-      
-      //ラジオボタンの選択変更時
-      const handleRadioChange = (value: number) => {
-        setSelectedValue(value);
-        setNum(0);
-        setMsg("　");
-      };
+  //ラジオボタンの選択変更時
+  const handleRadioChange = (value: number) => {
+    setSelectedValue(value);
+    onClickResetButton();
+  };
 
   return (
     <>
       <h2>Counter</h2>
-      
+
       <div>
         {/* UI */}
         <p>ボタンを押すとカウントアップ</p>
-        <RadioButton
-                label="1の倍数"
-                value={1}
-                checked={selectedValue === 1}
-                onChange={handleRadioChange}
-            />
+
+        {radioInfo.map((val) => {
+          return (
             <RadioButton
-                label="3の倍数"
-                value={3}
-                checked={selectedValue === 3}
-                onChange={handleRadioChange}
+              label={`${val.value}の倍数`}
+              value={val.value}
+              checked={selectedValue === val.value}
+              onChange={handleRadioChange}
             />
-            <RadioButton
-                label="5の倍数"
-                value={5}
-                checked={selectedValue === 5}
-                onChange={handleRadioChange}
-            />
-            <p>カウント数：{num}</p>
-            <p>{msg}</p>
-            <button onClick={onClickButton}>ボタン</button>
-            <button onClick={onClickResetButton}>リセット</button>
+          );
+        })}
+        <p>カウント数：{num}</p>
+        <p>{messageInfo[messageKey]}</p>
+        <button onClick={onClickButton}>ボタン</button>
+        <button onClick={onClickResetButton}>リセット</button>
       </div>
       <div>
-        <ul style={{
-          listStyle: "none",
-          padding: 20,
-        }}>
+        <ul
+          style={{
+            listStyle: "none",
+            padding: 20,
+          }}
+        >
           <li>
-            <Link to='/'>- Top -</Link>
+            <Link to="/">- Top -</Link>
           </li>
         </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default Counter
+export default Counter;
